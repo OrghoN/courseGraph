50377681a58724b21c7e92b257781b6539e7481c
OrghoN <anoronyo@gmail.com>
2017-02-14 12:39:38 -0600

    added text parser

---
+++ catalogueParser.js
@@ -0,0 +1,14 @@
+fs = require('fs')
+fs.readFile(process.argv[2], {
+    encoding: 'utf8',
+    normalize: true
+}, function(err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(parse(data));
+});
+
+function parse(data){
+  return data;
+}
